0ad69ed00428a96853a2c37d6ce99da1887c1aa3
AlexandrePerge <alexandre.perge@univ-tlse3.fr>
2020-03-26 11:39:56 +0100

    init spring boot project

---
+++ captain-adhoc-backend/src/test/java/captainadhocbackend/captainadhocbackend/CaptainAdhocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package captainadhocbackend.captainadhocbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptainAdhocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
