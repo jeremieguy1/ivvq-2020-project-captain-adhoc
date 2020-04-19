f1614f99f8f4e9121fe57b79f42739e72a529a57
Jérémie <mine.guy09@gmail.com>
2020-04-19 14:49:06 +0200

    [Front] - Utilisation de typescript

--- captain-adhoc-frontend/src/components/HelloWorld.vue
+++ captain-adhoc-frontend/src/components/HelloWorld.vue
@@ -2,14 +2,13 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For the a guide and recipes on how to configure / customize this project,<br>
+      For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -27,47 +26,16 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
-    <div>
-      <button v-on:click="mounted">Click me to test axios</button>
-      <p> {{ info }} </p>
-    </div>
-    
   </div>
-
 </template>
 
-<script>
-import {
-  HTTP
-} from '../http-common';
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  }, 
-  data () {
-    return {
-      info: "Init"
-    }
-    
-  },
-  methods: {
-    mounted: function() {
-      HTTP
-        .get('/test')
-        .then(response => {
-          this.info = response.data;
-        })
-        .error(error => {
-            console.log(error);
-        })
-    }
-  }
-  
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
 }
-
-
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
