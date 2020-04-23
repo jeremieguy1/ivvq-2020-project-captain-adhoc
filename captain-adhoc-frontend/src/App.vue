46f3a65b57c721f655d55150b7c1e822890ab03b
Jérémie <mine.guy09@gmail.com>
2020-04-23 17:15:53 +0200

    [Front] - Restructure le front sous un template webpack

--- captain-adhoc-frontend/src/App.vue
+++ captain-adhoc-frontend/src/App.vue
@@ -1,25 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+<script>
+export default {
+  name: 'App'
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
