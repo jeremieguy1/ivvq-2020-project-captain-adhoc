f6fab2c49c1818585e0cf5e907e62e2c7736444c
Jérémie <mine.guy09@gmail.com>
2020-05-01 21:48:07 +0200

    [Front] - Modifications pour les TU

--- captain-adhoc-frontend/test/unit/specs/Home.spec.js
+++ captain-adhoc-frontend/test/unit/specs/Home.spec.js
@@ -1,10 +1,40 @@
-// import Vue from 'vue'
-// import Home from '@/components/Home'
+import Vue from 'vue'
+import Home from '@/components/Home'
+import axios from 'axios'
+import moxios from 'moxios'
+import chai from 'chai'
 
 describe('Homme.vue', () => {
   it('should render correct contents', () => {
+    const Constructor = Vue.extend(Home)
+    const vm = new Constructor().$mount()
+    chai.expect(vm).to.have.property('info')
+  })
+
+  it('just for a single spec', function (done) {
     // const Constructor = Vue.extend(Home)
     // const vm = new Constructor().$mount()
-    // expect(vm).to.equal('')
+    // const fakeThis = {
+    //   info: null
+    // }
+    // const getData = vm.methods.getData.bind(fakeThis)
+    moxios.withMock(function () {
+      let onFulfilled = sinon.spy()
+      // getData.then(onFulfilled)
+      axios.get('/test').then(onFulfilled)
+
+      moxios.wait(function () {
+        let request = moxios.requests.mostRecent()
+        request.respondWith({
+          status: 200,
+          response: [
+            ['6']
+          ]
+        }).then(function () {
+          chai.expect(onFulfilled.called).to.equal(true)
+          done()
+        })
+      })
+    })
   })
 })
