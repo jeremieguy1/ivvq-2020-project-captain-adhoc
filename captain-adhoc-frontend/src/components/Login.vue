61ac56454ba28e410637b6a96281db870583d35c
Jérémie <mine.guy09@gmail.com>
2020-05-03 15:07:26 +0200

    [Front] - Nettoie les fichiers pour la PR

--- captain-adhoc-frontend/src/components/Login.vue
+++ captain-adhoc-frontend/src/components/Login.vue
@@ -1,44 +1,14 @@
 <template>
-    <div class="hero">
-    <div class="hero-body">
-          <h1 class="has-text-centered">Welcome to IVVQ app o/</h1>
-    </div>
-
-    <div class="is-flex rows">
-      <div class="row">
-        <button class="button is-primary has-text-centered"
-         v-on:click="getData">Click me to get stub data</button>
-      </div>
-      <div class="row">
-        <p v-if="info"> Info : {{ info }} </p>
-      </div>
-    </div>
-
-  </div>
+    <div> Login works !!</div>
 </template>
 
 <script>
-
-import {
-  HTTP
-} from '../http-common'
-
 export default {
   name: 'Login',
   data () {
-    return {
-      info: null
-    }
+    return {}
   },
-  methods: {
-    getData () {
-      HTTP
-        .get('/test')
-        .then(response => {
-          this.info = response.data
-        })
-    }
-  }
+  methods: {}
 }
 </script>
 
