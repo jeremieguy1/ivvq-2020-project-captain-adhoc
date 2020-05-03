98b7942b95f651af0a261b2063ee21a3e8e6d653
Jérémie <mine.guy09@gmail.com>
2020-05-03 20:30:50 +0200

    [Front] - Crée le composant navbar #23

--- captain-adhoc-frontend/src/App.vue
+++ captain-adhoc-frontend/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
+    <Navbar></Navbar>
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Navbar
+  }
 }
 </script>
 
 <style>
   @import "../node_modules/bulma/css/bulma.css";
+  @import "../node_modules/animate.css/animate.css";
 </style>
