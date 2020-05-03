6af15e68e2fffef9801fcb9a9b71dffc34fdf75b
FlorianPERU <florian.peru@univ-tlse3.fr>
2020-05-03 16:34:39 +0200

    [Front] Ajout de Rick et Tom Nook sur la page home

--- captain-adhoc-frontend/src/components/Home.vue
+++ captain-adhoc-frontend/src/components/Home.vue
@@ -1,8 +1,40 @@
 <template>
-<div>
+  <div>
+
     <div class="hero is-small is-primary">
       <div class="hero-body">
-        <h1>Welcome to the Captain Escroc market</h1>
+        <div class="columns">
+          <div class="column ">
+            <figure class="image is-4by5">
+              <img src="../assets/rickH.png" />
+            </figure>
+          </div>
+        <div class="column is-half">
+          <div class="level-item">
+            <h2 class="subtitle is-5">Welcome to the</h2>
+          </div>
+          <div class="level-item">
+            <h1 class="title is-1">Captain Escroc Market</h1>
+          </div>
+        </div>
+          <div class="column ">
+            <figure class="image is-4by5">
+              <img src="../assets/Tom_Nook.png" />
+            </figure>
+          </div>
+      </div>
+      </div>
+    </div>
+
+
+
+    <div class="container">
+
+      <div class="content is-medium">
+        <div class="column is-narrow has-text-centered">
+
+          <p>Si vous voulez accéder à nos escroqueries, veuillez vous connecter ou alors rejoignez nous !</p>
+        </div>
       </div>
     </div>
 
@@ -12,7 +44,7 @@
           <div class="level-item">
             <div class="buttons is-flex">
               <button class="button is-primary is-fullwidth login"
-                v-on:click="navigateToLogin">Login</button>
+                      v-on:click="navigate">Login</button>
               <button class="button is-primary is-fullwidth">Sign up</button>
             </div>
           </div>
