69062eac1931f14ab786df0f3fdb062aa559343d
FlorianPERU <florian.peru@univ-tlse3.fr>
2020-05-04 23:59:26 +0200

    [Front] Le carousel défile les images

--- captain-adhoc-frontend/src/components/Home.vue
+++ captain-adhoc-frontend/src/components/Home.vue
@@ -15,7 +15,7 @@
           <div class="level-item ">
             <h1 class="title is-1">Captain Escroc Market</h1>
           </div>
-          <vueper-slides id="slides" class="no-shadow" arrows-inside bullets-outside transition-speed="250" >
+          <vueper-slides id="slides" class="no-shadow" autoplay arrows-inside bullets-outside transition-speed="250" >
 
           <!--  <template v-slot:arrow-left>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" width="96px" height="96px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 14.5v-9l6 4.5-6 4.5z"/>
