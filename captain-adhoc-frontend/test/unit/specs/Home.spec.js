60ebc8ac70b8983b682b2929db28e8f154737f96
Jérémie <mine.guy09@gmail.com>
2020-05-07 21:27:05 +0200

    [Front] - Enlève l'utilisation dépréciée de la surcharge de 'methods' TU

--- captain-adhoc-frontend/test/unit/specs/Home.spec.js
+++ captain-adhoc-frontend/test/unit/specs/Home.spec.js
@@ -4,13 +4,9 @@ import { mount } from '@vue/test-utils'
 describe('Homme.vue', () => {
   it("Should call navigate method on click 'Login'", () => {
     let navigateToLoginSpy = sinon.spy()
-    let wrapper = mount(Home,
-      {
-        methods: {
-          navigateToLoginSpy
-        }
-      })
+    let wrapper = mount(Home)
 
+    navigateToLoginSpy.bind(Home, 'navigate')
     expect(wrapper.vm.navigateToLogin).to.be.a('function')
 
     expect(false, navigateToLoginSpy.calledOnce)
