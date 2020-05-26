fa86a1ff50e8537de04007679821cad5acd11648
FlorianPERU <florian.peru@univ-tlse3.fr>
2020-05-26 02:28:03 +0200

    [Front] Fix TU userManagement #74

---
+++ captain-adhoc-frontend/test/unit/specs/userManagement.spec.js
@@ -0,0 +1,40 @@
+import UserManagment from '@/components/userManagment'
+import moxios from 'moxios'
+
+const user = [{
+  id: 1,
+  is_admin: false,
+  mot_de_passe: 'motDepasse2',
+  nom: "'mister'",
+  nom_utilisateur: 'Hmister',
+  prenom: 'citoyen'
+}]
+
+describe('UserManagment.vue', () => {
+  beforeEach(() => {
+    moxios.install()
+  })
+
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  it('Should axios get default catch with error code', (done) => {
+    // Given
+
+    // When
+    UserManagment.getUser()
+
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: {
+          data: user
+        }
+      }).then(() => {
+        done()
+      })
+    })
+  })
+})
